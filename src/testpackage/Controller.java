632e3b1b3384541b6a71e286ebf51d87c7f001a8
Joseph Pong <oxjoe2006@yahoo.com>
2017-07-16 22:30:36 -0400

    updated controller, minor changes

--- src/testpackage/Controller.java
+++ src/testpackage/Controller.java
@@ -1,7 +1,6 @@
 package testpackage;
 
 import java.io.IOException;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,6 +18,12 @@ import org.jsoup.nodes.Document;
 /**
  * Created by Joseph on 7/6/2017.
  */
+
+
+// todo Bugs
+//  refreshTextfield and updateTextfield can't accept ""
+//  Numbers are all in ints
+
 public class Controller {
 
   @FXML // fx:id="linkTextfield"
@@ -27,6 +32,7 @@ public class Controller {
   @FXML // fx:id="backButton"
   private Button backButton; // Value injected by FXMLLoader
 
+  // Todo - change status text to show if input was accepted or not
   @FXML // fx:id="statusText"
   private Text statusText; // Value injected by FXMLLoader
 
@@ -48,6 +54,66 @@ public class Controller {
   @FXML // fx:id="githubLink"
   private Hyperlink githubLink; // Value injected by FXMLLoader
 
+  // Set title
+  // Fill in UI
+  // Add listeners to textfields
+  public void initialize() {
+    GimmeTheFreeStuff obj = new GimmeTheFreeStuff();
+    GetSetProps getSetProps = new GetSetProps();
+
+    addListeners();
+    try {
+      Main.getStage().setTitle("GimmeTheFreeStuff - " + obj.getTitle(getSetProps.getLink()));
+      fillInUI();
+      System.out.println("Success - Set title");
+      System.out.println("Success - Filled In UI");
+    } catch (IOException e) {
+      System.out.println("FAILED - Unable to set the title");
+      System.out.print("FAILED - Couldn't fill in UI");
+      e.printStackTrace();
+    }
+    System.out.println("*** Controller Initialized ***");
+  }
+
+  // Adds listeners to refreshTextField and updatesTextfield in case user changes them, but
+  // doesn't check the box
+  @FXML
+  void addListeners() {
+    GetSetProps obj = new GetSetProps();
+    // Listener for refreshTextfield
+    refreshTextfield.textProperty().addListener((observable, oldValue, newValue) -> {
+      try {
+        obj.setRefresh(Integer.parseInt(newValue));
+        System.out.println("Success - Added refresh listener to textfield");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
+    // Listener for updatesTextfield
+    updatesTextfield.textProperty().addListener((observable, oldValue, newValue) -> {
+      try {
+        obj.setUpdate(Integer.parseInt(newValue));
+        System.out.println("Success - Added update listener to textfield");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
+
+  }
+
+  // Fills in UI by getting properties from user.properties
+  @FXML
+  void fillInUI() throws IOException {
+    GetSetProps getSetProps = new GetSetProps();
+    currentLink.setText(getSetProps.getLink());
+    refreshTextfield.setText(Integer.toString(getSetProps.getRefresh()));
+    refreshCheckbox.setSelected(getSetProps.isRefreshChecked());
+    updatesTextfield.setText(Integer.toString(getSetProps.getUpdates()));
+    updatesCheckbox.setSelected(getSetProps.isUpdatesChecked());
+  }
+
+  // If checkbox is checked then save number of minutes/days and set corresponding property to
+  // true in user.properties
   @FXML
   void refreshListChecked() {
     if (refreshCheckbox.isSelected()) {
@@ -55,6 +121,7 @@ public class Controller {
       try {
         obj.setRefresh(Integer.parseInt(refreshTextfield.getText()));
         obj.setRefreshChecked(true);
+        System.out.println("Success - refresh settings are changed in file");
       } catch (IOException e) {
         e.printStackTrace();
       }
@@ -68,51 +135,11 @@ public class Controller {
       try {
         obj.setUpdate(Integer.parseInt(updatesTextfield.getText()));
         obj.setUpdatesChecked(true);
+        System.out.println("Success - update settings are changed in file");
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
-  }
-
-  public void initialize() {
-    GimmeTheFreeStuff obj = new GimmeTheFreeStuff();
-    GetSetProps getSetProps = new GetSetProps();
-    try {
-      Main.getStage().setTitle("GimmeTheFreeStuff for " + obj.getTitle(getSetProps.getLink()));
-    } catch (IOException e) {
-      System.out.println("Unable to set the title");
-      e.printStackTrace();
-    }
-    try {
-      fillInUI();
-    } catch (IOException e) {
-      System.err.print("Couldn't fill in UI");
-      e.printStackTrace();
-    }
-    System.out.println("*** Controller Initialized ***");
-  }
-
-  // Fills in ui with properties
-  void fillInUI() throws IOException {
-    GetSetProps getSetProps = new GetSetProps();
-    currentLink.setText(getSetProps.getLink());
-    refreshTextfield.setText(Integer.toString(getSetProps.getRefresh()));
-    refreshCheckbox.setSelected(getSetProps.isRefreshChecked());
-    updatesTextfield.setText(Integer.toString(getSetProps.getUpdates()));
-    updatesCheckbox.setSelected(getSetProps.isUpdatesChecked());
-    System.out.println("FINISHED FILL IN UI");
-  }
-
-
-  // Button to switch UI's
-  @FXML
-  void gotoMain() throws IOException {
-    Stage stage = (Stage) backButton.getScene().getWindow();
-    Parent root = FXMLLoader.load(getClass().getResource("MainUserInterface.fxml"));
-    Scene scene = new Scene(root);
-    stage.setScene(scene);
-    stage.show();
-    System.out.println("Switched to Main page");
   }
 
   // Gets the link from the TextField and tries to parse it with jSoup
@@ -121,22 +148,32 @@ public class Controller {
     GetSetProps getSetProps = new GetSetProps();
     String url = linkTextfield.getText();
     Document doc = obj.changeLink(url);
+    currentLink.setText(url);
     if (doc == null) {
-// TODO Let user knows that documetn couldn't parse by jSoup
+    // Todo, Important - Let user know that document couldn't parse by jSoup
     }
     try {
       getSetProps.setLink(url);
     } catch (IOException e) {
-      System.out.println("Couldn't save property");
+      System.out.println("FAILED - Couldn't save url property into file");
       e.printStackTrace();
     }
-    currentLink.setText(url);
   }
 
+  // Button to switch back to the Main UI
+  @FXML
+  void gotoMain() throws IOException {
+    Stage stage = (Stage) backButton.getScene().getWindow();
+    Parent root = FXMLLoader.load(getClass().getResource("MainUserInterface.fxml"));
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+    System.out.println("*** Switched Screens ***");
+  }
 
   // Opens GitHub in browser
   @FXML
-  void openGithub(ActionEvent event) {
+  void openGithub() {
     Main main = new Main();
     main.openUrl("https://github.com/oxjoe/GimmeTheFreeStuff");
   }
